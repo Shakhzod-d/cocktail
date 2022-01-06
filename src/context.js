9ae99c1db7460534051c59ba3031c0177d26e4d5
Shakhzod <forworkingappsinmylaptop@gmail.com>
2022-01-06 21:11:42 +0500

    working with SingleCocktail component and context.js

--- src/context.js
+++ src/context.js
@@ -9,31 +9,40 @@ const AppProvider = ({ children }) => {
 	const [ searchTerm, setSearchTerm ] = useState('a');
 	const [ cocktails, setCocktails ] = useState([]);
 
-	const fetchDrinks = async () => {
-		setLoading(true);
-		try {
-			const response = await fetch(`${url}${searchTerm}`);
-			const data = await response.json();
-			const { drinks } = data;
-			if (drinks) {
-				const newCocktails = drinks.map((item) => {
-					const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
-					return { id: idDrink, name: strDrink, image: strDrinkThumb, info: strAlcoholic, glass: strGlass };
-				});
-				setCocktails(newCocktails);
-			} else {
-				setCocktails([]);
+	const fetchDrinks = useCallback(
+		async () => {
+			setLoading(true);
+			try {
+				const response = await fetch(`${url}${searchTerm}`);
+				const data = await response.json();
+				const { drinks } = data;
+				if (drinks) {
+					const newCocktails = drinks.map((item) => {
+						const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+						return {
+							id: idDrink,
+							name: strDrink,
+							image: strDrinkThumb,
+							info: strAlcoholic,
+							glass: strGlass
+						};
+					});
+					setCocktails(newCocktails);
+				} else {
+					setCocktails([]);
+				}
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
 			}
-			setLoading(false);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+		},
+		[ searchTerm ]
+	);
 	useEffect(
 		() => {
 			fetchDrinks();
 		},
-		[ searchTerm ]
+		[ searchTerm, fetchDrinks ]
 	);
 
 	return <AppContext.Provider value={{ loading, setSearchTerm, cocktails }}>{children}</AppContext.Provider>;
