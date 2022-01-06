c18d203b82a0938ec13ba772ae71b5225b30f3cc
Shakhzod <forworkingappsinmylaptop@gmail.com>
2022-01-06 19:56:40 +0500

    fetching cocktails from and displaying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+// import components
+import Navbar from './components/Navbar';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/cocktail/:id" element={<SingleCocktail />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Projects are the most practical way to learn any language, andReact is no exception. Solidify your
+				knowledge of React by creating these cool projects. JOIN THE COURSE
+			</p>
+		</section>
+	);
+};
+
+export default About;
